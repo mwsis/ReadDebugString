fb95579672bb84dde76690d98eea29701e88e219
Matt Wilson <matthew.wilson.synesis@gmail.com>
2026-08-17 16:46:24 +1000

    chore: standardize shell-script color output

--- prepare_cmake.sh
+++ prepare_cmake.sh
@@ -4,6 +4,24 @@ ScriptPath=$0
 Dir=$(cd "$(dirname "$ScriptPath")" && pwd)
 ProjectNameFile="$Dir/.sis/project_name.txt"
 ProjectName=$(tr -d '[:space:]' < "$ProjectNameFile")
+
+# ##########################################################
+# colours
+
+if command -v tput > /dev/null; then
+
+  SisClr_Blue=${FG_BLUE:-$(tput setaf 4)}
+  SisClr_Red=${FG_RED:-$(tput setaf 1)}
+  SisClr_Bold=${FD_BOLD:-$(tput bold)}
+  SisClr_None=${FD_NONE:-$(tput sgr0)}
+else
+
+  SisClr_Blue=
+  SisClr_Red=
+  SisClr_Bold=
+  SisClr_None=
+fi
+
 CMakeDir=${SIS_CMAKE_BUILD_DIR:-$Dir/_build}
 Configuration=Release
 RunMake=0
@@ -18,12 +36,12 @@ while [[ $# -gt 0 ]]; do
       ;;
     --help)
       [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
-      printf 'Prepares the CMake build for %s\n\n' "$ProjectName"
+      printf "Prepares the CMake build for ${SisClr_Blue}${SisClr_Bold}%s${SisClr_None}\n\n" "$ProjectName"
       printf '%s [--debug-configuration|-d] [--run-make|-m] [--help]\n' "$ScriptPath"
       exit 0
       ;;
     *)
-      >&2 printf '%s: unrecognised argument %s; use --help for usage\n' "$ScriptPath" "$1"
+      >&2 printf "%s: ${SisClr_Red}${SisClr_Bold}unrecognised argument %s${SisClr_None}; use --help for usage\n" "$ScriptPath" "$1"
       exit 1
       ;;
   esac
